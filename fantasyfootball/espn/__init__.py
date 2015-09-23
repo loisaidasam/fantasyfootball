467a060edde5a188110589f6cf1bb23f5d9bde34
Sam Sandberg <sam.sandberg@gmail.com>
2015-09-22 21:45:33 -0400

    Team parsing

--- fantasyfootball/espn/__init__.py
+++ fantasyfootball/espn/__init__.py
@@ -55,28 +55,57 @@ class ESPNTeam(BaseTeam):
         # For now, using this cookie method
         self.cookie = cookie
 
-    def get_players(self):
-        """Method for getting players on your team
-
-        In the JS console as of 2013-08-29:
-        document.getElementsByClassName("pncPlayerRow")[0].getElementsByClassName("playertablePlayerName")[0].getElementsByTagName("a")[0].text
+    def _get_players_soup(self):
+        """Helpful for debugging
         """
         url = TEAM_URL_TEMPLATE % (self.league_id,
                                    self.team_id,
                                    self.season_id)
         response = self.session.get(url, headers={'Cookie': self.cookie})
-        soup = BeautifulSoup(response.content, "lxml")
+        return BeautifulSoup(response.content, "lxml")
+
+    def get_players(self):
+        """Method for getting players on your team
+        """
+        soup = self._get_players_soup()
         players = []
         for player_row in soup.find_all('tr', "pncPlayerRow"):
             player_cols = player_row.find_all('td')
-            if not all([player_cols, player_cols[0], player_cols[1]]):
+            if not player_cols:
                 continue
-            # Their name:
-            pos = player_cols[0].text
-            name_anchor = player_cols[1].find('a')
-            name = name_anchor and name_anchor.text
+            player_info = player_cols[1].text
+            player_info = player_info and player_info.strip()
+            if not player_info:
+                continue
+            slot = player_cols[0] and player_cols[0].text
+            if ',' in player_info:
+                # Player
+                name, remainder = player_info.split(', ')
+                pieces = remainder.split()
+                team = pieces[0]
+                pos = pieces[1]
+                status = len(pieces) > 2 and pieces[2] or u"OK"
+            else:
+                # DST
+                name1, name2, _ = player_info.split()
+                name = u" ".join([name1, name2])
+                status = u"OK"
             players.append({
                 'name': name,
                 'pos': pos,
+                'team': team,
+                'slot': slot,
+                'status': status,
+                'opp': player_cols[4].text,
+                'status_et': player_cols[5].text,
+                'prk': player_cols[7].text,
+                'pts': player_cols[8].text,
+                'avg': player_cols[9].text,
+                'last': player_cols[10].text,
+                'proj': player_cols[12].text,
+                'oprk': player_cols[13].text,
+                'pct_st': player_cols[14].text,
+                'pct_own': player_cols[15].text,
+                'plus_minus': player_cols[16].text,
             })
         return players
