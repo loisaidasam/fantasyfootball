54a17f98bc31058847f2c1cb3e5281efa439a3f0
Sam Sandberg <sam.sandberg@gmail.com>
2016-09-12 22:53:31 -0400

    Gearing up for 2016
    Checking in leftover changes from 2015 season
    Some cleanup/special case handling
    espn.com now (no longer espn.go.com)
    Updated libs in requirements

---
+++ export.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import argparse
+import csv
+import datetime
+import logging
+
+from fantasyfootball.espn import ESPNTeam
+
+import settings
+
+
+logger = logging.getLogger('')
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s (%(process)d): %(message)s',
+                              '%Y-%m-%d %H:%M:%S')
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
+def write_data(filename, players):
+    with open(filename, 'w') as fp:
+        writer = csv.writer(fp)
+        for i, player in enumerate(players):
+            if i == 0:
+                header_row = player.keys()
+                writer.writerow(header_row)
+            row = []
+            for key in header_row:
+                row.append(player.get(key))
+            writer.writerow(row)
+    logger.info("Wrote %s players to %s", len(players), filename)
+
+
+def main():
+    # parser = argparse.ArgumentParser(description="Grab/print the team members")
+    # parser.add_argument('url', type=str, help="")
+    # args = parser.parse_args()
+    # params = ESPNTeam.parse_params_from_url(args.url)
+    # settings.ESPN_URL is something like this:
+    # url = "http://games.espn.com/ffl/freeagency?leagueId=<YOUR_LEAGUE_ID>&teamId=<YOUR_TEAM_ID>&seasonId=<THIS_YEAR>"
+    url = settings.ESPN_URL
+    params = ESPNTeam.parse_params_from_url(url)
+    team = ESPNTeam(**params)
+    # To get `settings.ESPN_COOKIE`, in a browser (tested w/ Chrome), open the
+    # JS console and click the "Network" tab. Limit the requests to only "XHR"
+    # requests. Now open your ESPN Fantasy League page and click the "Players"
+    # tab. A ton of resources will probably load - wait until they've all loaded
+    # and then clear them all. At the bottom of the players list, click "Next",
+    # and you'll see the one resource that loads - THIS is the one you want.
+    # Right click and choose "Copy as cURL". Paste the result somewhere and nab
+    # the part that says "-H 'Cookie: FFL_LM_COOKIE= ...". This is what you are
+    # looking for.
+    team.set_cookie(settings.ESPN_COOKIE)
+
+    # print players
+    # for i, player in enumerate(players, start=1):
+    #     print "%d.\t%s" % (i, player)
+    # write_data('data/my_team.csv', team.get_team())
+
+    # In [3]: datetime.datetime.now().isoformat().split('.')[0][:-3].replace('T', '-').replace(':', '-')
+    # Out[3]: '2016-09-12-17-24'
+    date_str = datetime.datetime.now().isoformat().split('.')[0][:-3].replace('T', '-').replace(':', '-')
+    filename = 'data/players-%s.csv' % date_str
+    write_data(filename, team.get_players())
+
+
+if __name__ == '__main__':
+    main()
--- fantasyfootball/espn/__init__.py
+++ fantasyfootball/espn/__init__.py
@@ -8,10 +8,10 @@ import requests
 
 from fantasyfootball.base_team import BaseTeam
 
-LOGIN_URL_GET = 'http://games.espn.go.com/frontpage/football'
+LOGIN_URL_GET = 'http://games.espn.com/frontpage/football'
 LOGIN_URL_POST = 'https://registerdisney.go.com/jgc/v2/client/ESPN-FANTASYLM-PROD/guest/login?langPref=en-US'
-TEAM_URL_TEMPLATE = 'http://games.espn.go.com/ffl/clubhouse?leagueId=%s&teamId=%s&seasonId=%s'
-PLAYERS_URL_TEMPLATE = 'http://games.espn.go.com/ffl/freeagency?leagueId=%s&teamId=%s&seasonId=%s#&seasonId=%s&context=freeagency&view=overview&startIndex=%s'
+TEAM_URL_TEMPLATE = 'http://games.espn.com/ffl/clubhouse?leagueId=%s&teamId=%s&seasonId=%s'
+PLAYERS_URL_TEMPLATE = 'http://games.espn.com/ffl/playertable/prebuilt/freeagency?leagueId=%s&teamId=%s&seasonId=%s&&context=freeagency&view=overview&startIndex=%s'
 
 
 logger = logging.getLogger(__name__)
@@ -22,6 +22,8 @@ class InvalidPlayerIdError(Exception):
 
 
 class ESPNTeam(BaseTeam):
+    PLAYER_INFO_ADV_OPP_BYE = '** BYE **'
+
     def __init__(self, league_id, team_id, season_id):
         self.league_id = league_id
         self.team_id = team_id
@@ -30,7 +32,7 @@ class ESPNTeam(BaseTeam):
 
     @staticmethod
     def parse_params_from_url(url):
-        match = re.match(r"http://games\.espn\.go\.com/ffl/(clubhouse|freeagency)\?leagueId=(\d+)\&teamId=(\d+)\&seasonId=(\d+)",
+        match = re.match(r"http://games\.espn\.com/ffl/(clubhouse|freeagency)\?leagueId=(\d+)\&teamId=(\d+)\&seasonId=(\d+)",
                          url)
         if not match:
             raise Exception("Invalid URL")
@@ -41,7 +43,7 @@ class ESPNTeam(BaseTeam):
         }
 
     def login(self, email, password):
-        # TODO: Work out login
+        # TODO: Work out login - for now relying on cookies method
         self.session.get(LOGIN_URL_GET)
         payload = {'loginValue': email, 'password': password}
         headers = {'Content-type': 'application/json;charset=UTF-8'}
@@ -65,10 +67,14 @@ class ESPNTeam(BaseTeam):
         response = self.session.get(url, headers={'Cookie': self.cookie})
         return BeautifulSoup(response.content, "lxml")
 
-    def _parse_basic_player_info(self, soup):
-        if ',' in soup:
+    def _parse_player_info_basic(self, player_info_str):
+        """http://stackoverflow.com/questions/4995116/only-extracting-text-from-this-element-not-its-children/4995480#4995480
+        print col.find(text=True, recursive=False)
+        """
+        if ',' in player_info_str:
             # Player
-            name, remainder = soup.split(', ')
+            # Ex: `Ryan Fitzpatrick, NYJ QB`
+            name, remainder = player_info_str.split(', ', 1)
             pieces = remainder.split()
             team = pieces[0]
             pos = pieces[1]
@@ -80,19 +86,58 @@ class ESPNTeam(BaseTeam):
                 'status': status,
             }
         # D/ST
-        name1, name2, pos = soup.split()
+        # Ex: `Browns D/ST D/ST`
+        name1, name2, pos = player_info_str.split()
         return {
             'name': u" ".join([name1, name2]),
+            # TODO: Fill in team
+            # (Player has team listed next to their name like: `NYJ` but D/ST
+            # have full team name listed like: `Browns`)
+            'team': None,
             'pos': pos,
             'status': u"OK",
         }
 
+    def _parse_player_info_advanced(self, player_cols):
+        result = {'status': player_cols[2].text}
+        opp = player_cols[5].text
+        if '@' in opp:
+            result['opp'] = opp[1:]
+            result['home_away'] = 'AWAY'
+        else:
+            result['opp'] = opp
+            result['home_away'] = 'HOME'
+        index_adj = 0
+        if result['opp'] == ESPNTeam.PLAYER_INFO_ADV_OPP_BYE:
+            result['opp'] = 'BYE'
+            result['status_et'] = None
+            index_adj = -1
+        else:
+            result['status_et'] = player_cols[6].text
+        result.update({
+            'prk': player_cols[8 + index_adj].text,
+            'pts': player_cols[9 + index_adj].text,
+            'avg': player_cols[10 + index_adj].text,
+            'last': player_cols[11 + index_adj].text,
+            'proj': player_cols[13 + index_adj].text,
+            'oprk': player_cols[14 + index_adj].text,
+            'pct_st': player_cols[15 + index_adj].text,
+            'pct_own': player_cols[16 + index_adj].text,
+            'plus_minus': player_cols[17 + index_adj].text,
+        })
+        return result
+
     def get_team(self):
         """Method for getting players on your team
         """
+        logger.info("get_team()")
         soup = self._get_team_soup()
         players = []
-        for player_row in soup.find_all('tr', "pncPlayerRow"):
+        player_rows = soup.find_all('tr', "pncPlayerRow")
+        for player_num, player_row in enumerate(player_rows, start=1):
+            logger.info("Grabbing player %s of %s ...",
+                        player_num,
+                        len(player_rows))
             player_cols = player_row.find_all('td')
             if not player_cols:
                 continue
@@ -101,7 +146,7 @@ class ESPNTeam(BaseTeam):
             if not player_info:
                 continue
             slot = player_cols[0] and player_cols[0].text
-            player = self._parse_basic_player_info(player_info)
+            player = self._parse_player_info_basic(player_info)
             player.update({
                 'slot': slot,
                 'opp': player_cols[4].text,
@@ -124,17 +169,28 @@ class ESPNTeam(BaseTeam):
         url = PLAYERS_URL_TEMPLATE % (self.league_id,
                                       self.team_id,
                                       self.season_id,
-                                      self.season_id,
                                       offset)
+        logger.info("URL: %s", url)
         response = self.session.get(url, headers={'Cookie': self.cookie})
         return BeautifulSoup(response.content, "lxml")
 
-    def get_players(self):
-        offset = 0
+    def get_players(self, max_num_requests=None):
+        logger.info("get_players()")
+        offset = num_requests = 0
         players = []
+        players_seen = set()
         while True:
+            if max_num_requests and num_requests >= max_num_requests:
+                logger.info("Hit max_num_requests of %s", max_num_requests)
+                break
+            players_this_time = 0
             soup = self._get_players_soup_piece(offset)
-            for player_row in soup.find_all('tr', "pncPlayerRow"):
+            num_requests += 1
+            player_rows = soup.find_all('tr', "pncPlayerRow")
+            for player_num, player_row in enumerate(player_rows, start=1):
+                logger.info("Grabbing player %s of %s ...",
+                            player_num,
+                            len(player_rows))
                 player_cols = player_row.find_all('td')
                 if not player_cols:
                     continue
@@ -142,22 +198,30 @@ class ESPNTeam(BaseTeam):
                 player_info = player_info and player_info.strip()
                 if not player_info:
                     continue
-                player = self._parse_basic_player_info(player_info)
-                player.update({
-                    'status': player_cols[2].text,
-                    'opp': player_cols[5].text,
-                    'status_et': player_cols[6].text,
-                    'prk': player_cols[8].text,
-                    'pts': player_cols[9].text,
-                    'avg': player_cols[10].text,
-                    'last': player_cols[11].text,
-                    'proj': player_cols[13].text,
-                    'oprk': player_cols[14].text,
-                    'pct_st': player_cols[15].text,
-                    'pct_own': player_cols[16].text,
-                    'plus_minus': player_cols[17].text,
-                })
+                try:
+                    player = self._parse_player_info_basic(player_info)
+                except:
+                    logger.exception("Error parsing player_info: %s",
+                                     player_info)
+                    raise
+                try:
+                    player_info_advanced = self._parse_player_info_advanced(player_cols)
+                except:
+                    logger.exception("Error parsing advanced player info. player_cols: %s /// player_row: %s",
+                                     player_cols,
+                                     player_row)
+                    raise
+                player.update(player_info_advanced)
                 players.append(player)
+                player_hash = (player['name'], player['team'])
+                if player_hash in players_seen:
+                    logger.warning("We already saw %s !", player_hash)
+                    return players
+                players_seen.add(player_hash)
+                players_this_time += 1
+            logger.info("Offset: %s, got %s players", offset, players_this_time)
+            if players_this_time < 50:
+                logger.info("Finishing up")
+                break
             offset += 50
-            return players
         return players
